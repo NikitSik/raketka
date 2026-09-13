c2098bcb3ce78ad7c7a620e42b76a3f0b508bef7
NikitSik <nikitafateev777@gmail.com>
2026-09-13 20:17:51 +0400

    vers01

--- src/main/java/com/crash/Raketka/config/WebConfig.java
+++
@@ -1,7 +0,0 @@
-package com.crash.Raketka.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig {
-}
--- src/main/java/com/crash/Raketka/domain/Reward.java
+++
@@ -1,8 +0,0 @@
-package com.crash.Raketka.domain;
-
-public enum Reward {
-    PUZZLE_A,
-    PUZZLE_B,
-    PUZZLE_C,
-    PUZZLE_D
-}
